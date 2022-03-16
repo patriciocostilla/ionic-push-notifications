3fff1d6a971a75122266910d19049dc1bd20bc64
Patricio Costilla <juan.costilla@santexgroup.com>
2022-03-16 12:30:36 -0300

    creating notification service

---
+++ pushApp/src/app/services/fcm.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FcmService {
+  constructor(private platform: Platform) {}
+
+  init() {
+    if (!this.platform.is('capacitor')) {
+      console.log("App running from web browser. Won't connect to firebase");
+      return;
+    }
+
+    PushNotifications.requestPermissions().then((result) => {
+      // Request permission to use push notifications. In Android, this defaults to 'granted'
+      if (result.receive === 'granted') {
+        console.log('Starting firebase register');
+        PushNotifications.register();
+      }
+    });
+
+    // Triggered by PushNotifications.register()
+    PushNotifications.addListener('registration', (token: Token) => {
+      console.log('Push registration success, token: ' + token.value);
+    });
+
+    // Triggered when 'registration' listener fails
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.log('Error on registration: ' + JSON.stringify(error));
+    });
+
+    // Triggered when a notification is received and the app is open
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        console.log(
+          'Push notification received: ' + JSON.stringify(notification)
+        );
+      }
+    );
+
+    // Triggered when tapping on a notification
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        console.log(
+          console.log('Push action performed: ' + JSON.stringify(notification))
+        );
+      }
+    );
+  }
+}
